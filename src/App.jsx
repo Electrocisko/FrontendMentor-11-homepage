80c891ddb4456bb1ff5e99210a62ec555cb9a8d4
Francisco Zuidwijk <electrocisko@gmail.com>
2023-12-19 08:29:19 -0300

    navbar whit bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import NavBar from "./components/NavBar";
 import MainCard from "./components/MainCard";
 import Aside from "./components/Aside";
 import Card from "./components/Card";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 import "./App.css";
 
