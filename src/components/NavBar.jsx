3771045df4f5d4867f3d78ab7d1eb1df00e7683e
Francisco Zuidwijk <electrocisko@gmail.com>
2023-12-20 16:42:22 -0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,19 +12,19 @@ function NavBar() {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ms-auto">
         <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="#">Home</a>
+          <a className="nav-link ms-3" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">New</a>
+          <a className="nav-link ms-3" href="#">New</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Popular</a>
+          <a className="nav-link ms-3" href="#">Popular</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Trending</a>
+          <a className="nav-link ms-3" href="#">Trending</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Categories</a>
+          <a className="nav-link ms-3" href="#">Categories</a>
         </li>
 
       </ul>
